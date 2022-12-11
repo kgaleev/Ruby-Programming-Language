7a2e4b30c16e0e1cb4c0c4318e105e0ad21717c6
kgaleev <kkgaleev@gmail.com>
2022-12-11 10:59:47 +0500

    added Proc

--- small_progs.rb
+++ small_progs.rb
@@ -30,3 +30,20 @@ end
 puts "#{t = Test.new 2}"
 puts
 ###############################################################
+greet = Proc.new do |x|
+  puts "Welcome #{x}"
+end
+
+goodbye = Proc.new do |x|
+  puts "Goodbye #{x}"
+end
+
+def say(arr, proc)
+  arr.each { |x| proc.call x}
+end
+
+people = ["David", "Amy", "John"]
+say(people, greet)
+say(people, goodbye)
+puts
+###############################################################
